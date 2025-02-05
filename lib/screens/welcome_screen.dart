860ab1b5c299c7894c63a0bae3936cf19b7b3c7a
phritnakgate <boss5500216@gmail.com>
2025-02-05 22:41:33 +0700

    feat(guest): select gender

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bazi_app_frontend/screens/guesthora_screen.dart';
+import 'package:bazi_app_frontend/widgets/gender_selector_widget.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController dateController = TextEditingController();
   TextEditingController timeController = TextEditingController();
+  int selectedGender = 0; //0 = Male, 1 = Female
 
   @override
   Widget build(BuildContext context) {
@@ -43,133 +45,145 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   onTap: () => showDialog(
                       context: context,
                       builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Center(
-                              child: Text(
-                            "ใส่ข้อมูลของท่าน",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          )),
-                          content: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.65,
-                                child: TextField(
-                                  cursorColor: Theme.of(context).focusColor,
-                                  decoration: const InputDecoration(
-                                    labelText: "ชื่อ",
-                                  ),
-                                  controller: nameController,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Row(
+                        int selectedGender = 0;
+                        return StatefulBuilder(
+                          builder: (context, setDialogState) {
+                            return AlertDialog(
+                              title: const Center(
+                                  child: Text(
+                                "ใส่ข้อมูลของท่าน",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              )),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  GestureDetector(
-                                    onTap: () async {
-                                      final DateTime? pickedDate =
-                                          await showDatePicker(
-                                              context: context,
-                                              firstDate: DateTime(1900),
-                                              lastDate: DateTime.now());
-                                      if (pickedDate != null &&
-                                          pickedDate != DateTime.now()) {
-                                        setState(() {
-                                          dateController.text =
-                                              "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
-                                        });
-                                      }
-                                    },
-                                    child: SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.3,
-                                      child: TextField(
-                                        controller: dateController,
-                                        enabled: false,
-                                        decoration: const InputDecoration(
-                                            labelText: "เลือกวันเกิด"),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.65,
+                                    child: TextField(
+                                      cursorColor: Theme.of(context).focusColor,
+                                      decoration: const InputDecoration(
+                                        labelText: "ชื่อ",
                                       ),
+                                      controller: nameController,
                                     ),
                                   ),
                                   const SizedBox(
-                                    width: 10,
+                                    height: 15,
+                                  ),
+                                  Row(
+                                    spacing: 10,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () async {
+                                          final DateTime? pickedDate =
+                                              await showDatePicker(
+                                                  context: context,
+                                                  firstDate: DateTime(1900),
+                                                  lastDate: DateTime.now());
+                                          if (pickedDate != null &&
+                                              pickedDate != DateTime.now()) {
+                                            setState(() {
+                                              dateController.text =
+                                                  "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                                            });
+                                          }
+                                        },
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.3,
+                                          child: TextField(
+                                            controller: dateController,
+                                            enabled: false,
+                                            decoration: const InputDecoration(
+                                                labelText: "เลือกวันเกิด"),
+                                          ),
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () async {
+                                          final TimeOfDay? timePicked =
+                                              await showTimePicker(
+                                                  context: context,
+                                                  initialTime: TimeOfDay.now(),
+                                                  builder: (context, child) {
+                                                    return MediaQuery(
+                                                        data: MediaQuery.of(
+                                                                context)
+                                                            .copyWith(
+                                                                alwaysUse24HourFormat:
+                                                                    true),
+                                                        child: child!);
+                                                  });
+                                          if (timePicked != null &&
+                                              timePicked != TimeOfDay.now()) {
+                                            setState(() {
+                                              timeController.text =
+                                                  "${timePicked.hour}:${timePicked.minute}";
+                                            });
+                                          }
+                                        },
+                                        child: SizedBox(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.3,
+                                          child: TextField(
+                                            controller: timeController,
+                                            enabled: false,
+                                            decoration: const InputDecoration(
+                                                labelText: "เลือกเวลาเกิด"),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 15,
+                                  ),
+                                  GenderSelectorWidget(
+                                    onGenderSelected: (gender) {
+                                      setDialogState(() {
+                                        selectedGender = gender;
+                                      });
+                                    },
+                                  ),
+                                  const SizedBox(
+                                    height: 30,
                                   ),
                                   GestureDetector(
-                                    onTap: () async {
-                                      final TimeOfDay? timePicked =
-                                          await showTimePicker(
-                                              context: context,
-                                              initialTime: TimeOfDay.now(),
-                                              builder: (context, child) {
-                                                return MediaQuery(
-                                                    data: MediaQuery.of(context)
-                                                        .copyWith(
-                                                            alwaysUse24HourFormat:
-                                                                true),
-                                                    child: child!);
-                                              });
-                                      if (timePicked != null &&
-                                          timePicked != TimeOfDay.now()) {
-                                        setState(() {
-                                          timeController.text =
-                                              "${timePicked.hour}:${timePicked.minute}";
-                                        });
-                                      }
-                                    },
-                                    child: SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.3,
-                                      child: TextField(
-                                        controller: timeController,
-                                        enabled: false,
-                                        decoration: const InputDecoration(
-                                            labelText: "เลือกเวลาเกิด"),
-                                      ),
-                                    ),
-                                  ),
+                                      onTap: () {
+                                        Navigator.pop(context);
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const GuestHoraScreen()));
+                                      },
+                                      child: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.75,
+                                        height: 40,
+                                        decoration: BoxDecoration(
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                            borderRadius:
+                                                BorderRadius.circular(15)),
+                                        child: const Center(
+                                            child: Text(
+                                          "วิเคราะห์พื้นดวง",
+                                          style: TextStyle(color: wColor),
+                                        )),
+                                      ))
                                 ],
                               ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Row(
-                                children: [
-                                  Text("เพศ"),
-                                  Container(),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 30,
-                              ),
-                              GestureDetector(
-                                  onTap: () {
-                                    Navigator.pop(context);
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const GuestHoraScreen()));
-                                  },
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width *
-                                        0.75,
-                                    height: 40,
-                                    decoration: BoxDecoration(
-                                        color: Theme.of(context).primaryColor,
-                                        borderRadius:
-                                            BorderRadius.circular(15)),
-                                    child: const Center(
-                                        child: Text(
-                                      "วิเคราะห์พื้นดวง",
-                                      style: TextStyle(color: wColor),
-                                    )),
-                                  ))
-                            ],
-                          ),
+                            );
+                          },
                         );
                       }),
                   child: Container(
